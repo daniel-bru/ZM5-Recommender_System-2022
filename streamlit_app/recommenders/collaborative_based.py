208e98072c7f4d2b45989c4429b54353861d2999
daniel-bru <81396537+daniel-bru@users.noreply.github.com>
2021-07-15 22:46:07 +0200

    Fixed Algorithm

--- streamlit_app/recommenders/collaborative_based.py
+++ streamlit_app/recommenders/collaborative_based.py
@@ -36,6 +36,7 @@ from surprise import Reader, Dataset
 from surprise import SVD, NormalPredictor, BaselineOnly, KNNBasic, NMF
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+import random
 
 # Importing data
 movies_df = pd.read_csv('resources/data/movies.csv')
@@ -119,30 +120,23 @@ def collab_model(movie_list,top_n=10):
     """
 
     indices = pd.Series(movies_df['title'])
-    movie_ids = pred_movies(movie_list)
-    df_init_users = ratings_df[ratings_df['userId']==movie_ids[0]]
-    for i in movie_ids :
+    user_ids = pred_movies(movie_list)
+
+    # Create Dataframe of users with similar high ratings for each movie
+    df_init_users = ratings_df[ratings_df['userId']==user_ids[0]]
+    for i in user_ids[1:]:
         df_init_users=df_init_users.append(ratings_df[ratings_df['userId']==i])
-    # Getting the cosine similarity matrix
-    cosine_sim = cosine_similarity(np.array(df_init_users), np.array(df_init_users))
-    idx_1 = indices[indices == movie_list[0]].index[0]
-    idx_2 = indices[indices == movie_list[1]].index[0]
-    idx_3 = indices[indices == movie_list[2]].index[0]
-    # Creating a Series with the similarity scores in descending order
-    rank_1 = cosine_sim[idx_1]
-    rank_2 = cosine_sim[idx_2]
-    rank_3 = cosine_sim[idx_3]
-    # Calculating the scores
-    score_series_1 = pd.Series(rank_1).sort_values(ascending = False)
-    score_series_2 = pd.Series(rank_2).sort_values(ascending = False)
-    score_series_3 = pd.Series(rank_3).sort_values(ascending = False)
-     # Appending the names of movies
-    listings = score_series_1.append(score_series_1).append(score_series_3).sort_values(ascending = False)
-    recommended_movies = []
-    # Choose top 50
-    top_50_indexes = list(listings.iloc[1:50].index)
-    # Removing chosen movies
-    top_indexes = np.setdiff1d(top_50_indexes,[idx_1,idx_2,idx_3])
-    for i in top_indexes[:top_n]:
-        recommended_movies.append(list(movies_df['title'])[i])
+
+    df_init_users = df_init_users.merge(movies_df[['movieId','title']], on='movieId')
+
+    # Get the top rated movies
+    sorted_movies = df_init_users.groupby('title').mean().sort_values(by='rating', ascending=False)
+
+    no_movies = sorted_movies[sorted_movies['rating']==5].shape[0]
+
+    good_movies = sorted_movies.index[:no_movies].to_list()
+
+    # Sample from the top n movies and recommend 
+    recommended_movies = random.sample(good_movies, top_n)
+
     return recommended_movies
